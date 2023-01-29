e4d015abe61d04a47eb9dc5beeb028f3958e8248
Eduardo Ribeiro da Silva <filosofisto@users.noreply.github.com>
2023-01-29 11:58:01 +0100

    high_resolution_clock_01 project

---
+++ Time/high_resolution_clock_01/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <chrono>
+#include <cmath>
+
+using namespace std;
+using namespace chrono;
+
+int main() {
+    auto start { high_resolution_clock::now() };
+
+    double d { 0 };
+    for (int i { 0 }; i < 1'000'000; ++i) {
+        d += sqrt(sin(i) * cos(i));
+    }
+    
+    auto end { high_resolution_clock ::now() };
+    auto diff { end - start };
+
+    cout << duration<double, milli> { diff }.count() << "ms" << endl;
+    return EXIT_SUCCESS;
+}
