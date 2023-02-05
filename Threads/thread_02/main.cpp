53b0b179aabc8b12215b73c5428b98d6d4001697
Eduardo Ribeiro da Silva <filosofisto@users.noreply.github.com>
2023-02-05 09:56:16 +0100

    execute on real object instead the copy

--- Threads/thread_02/main.cpp
+++ Threads/thread_02/main.cpp
@@ -5,13 +5,15 @@
 using std::cout;
 using std::endl;
 using std::thread;
+using std::ref;
 
 int main() {
     thread t1 { Counter{ 1, 20 } };
 
     Counter c { 2, 20 };
-    thread t2 { c };
+//    thread t2 { c };
 
+    thread t2 { ref(c) }; // to execute on actual object instead of copied one
     t1.join();
     t2.join();
 
