494fcd4798ea1910f01c83708bebccc809fe6a94
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2023-12-15 01:37:44 -0800

    boost_optional01 project

---
+++ Boost/boost_optional01/main.cpp
@@ -0,0 +1,55 @@
+#include <boost/optional.hpp>
+#include <iostream>
+
+class locked_device
+{
+    explicit locked_device(const char* /* param */) 
+    {
+        std::cout << "Device is locked\n";
+    }
+
+    static bool try_lock_device_impl()
+    {
+        std::cout << "Lock implementation running...\n";
+        return true;
+    }
+    
+public:
+    void use()
+    {
+        std::cout << "Success!\n";
+    }
+    
+    static boost::optional<locked_device> try_lock_device()
+    {
+        if (!try_lock_device_impl()) {
+            // Failed to lock device
+            return boost::none;
+        }
+        
+        // Success!
+        return locked_device("device name");
+    }
+    
+    ~locked_device()
+    {
+        // Release device lock
+    }
+};
+
+int main()
+{
+    for (unsigned i = 0; i < 10; ++i) {
+        boost::optional<locked_device> t = locked_device::try_lock_device();
+        
+        if (t) {
+            t->use();
+            return EXIT_SUCCESS;
+        } else {
+            std::cout << "...trying again\n";
+        }
+    }
+    
+    std::cout << "Failure!\n";
+    return EXIT_FAILURE;
+}
